f97a1664c804e25177b81e5c34ff7b1640dfe02f
Maxim Kovalev <maxim.kovalev@west.cmu.edu>
2013-09-23 14:04:03 -0700

    Now it has a textview.

--- MySandbox/src/edu/cmu/west/mysandbox/MainActivity.java
+++ MySandbox/src/edu/cmu/west/mysandbox/MainActivity.java
@@ -3,13 +3,20 @@ package edu.cmu.west.mysandbox;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+//import com.google.gson.Gson;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
+	
+	TextView hellotext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+    
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        hellotext = (TextView)findViewById(R.id.hellotext);
+        hellotext.setText("Ololo");
     }
 
 
