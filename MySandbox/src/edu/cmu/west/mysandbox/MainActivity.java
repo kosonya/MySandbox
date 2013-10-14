754590bf87a8e5d6ad912cbc17075cfd21e51f64
Maxim Kovalev <maxim.kovalev@west.cmu.edu>
2013-10-14 14:28:38 -0700

    Now it has GPS and call count.

--- MySandbox/src/edu/cmu/west/mysandbox/MainActivity.java
+++ MySandbox/src/edu/cmu/west/mysandbox/MainActivity.java
@@ -1,5 +1,8 @@
 package edu.cmu.west.mysandbox;
 
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.app.Activity;
 import android.widget.TextView;
@@ -7,6 +10,7 @@ import android.content.Intent;
 import android.content.Context;
 import android.content.IntentFilter;
 import java.lang.Float;
+import java.math.BigInteger;
 
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -21,15 +25,20 @@ import java.util.List;
 
 public class MainActivity extends Activity implements SensorEventListener {
 	
-	public TextView pressureTV, humidityTV, accelerometerTV, temperatureTV, lightTV;
+	public TextView pressureTV, humidityTV, accelerometerTV, temperatureTV,
+					lightTV, longitudeTV, latitudeTV, altitudeTV, bearingTV, accuracyTV, callcountTV;
 	Intent batteryStatus;
 	Context context;
 	IntentFilter ifilter;
 	SensorManager sensormanager;
 	List<Sensor> sensorlist;
 	Sensor humidityS, pressureS, accelerometerS, temperatureS, lightS;
-	int callcount;
+	BigInteger callcount;
 	List<Float> pressurevals, humidityvals, accelerometervals, temperaturevals, lightvals;
+	List<Double> gpsvals;
+	LocationListener gpslistener;
+	Boolean gps_is_enabled;
+	LocationManager locman;
 
 
 
@@ -46,6 +55,12 @@ public class MainActivity extends Activity implements SensorEventListener {
         accelerometerTV = (TextView)findViewById(R.id.accTV);
         temperatureTV = (TextView)findViewById(R.id.temperatureTV);
         lightTV = (TextView)findViewById(R.id.lightTV);
+        latitudeTV = (TextView)findViewById(R.id.latitudeTV);
+        longitudeTV = (TextView)findViewById(R.id.longitudeTV);
+        altitudeTV = (TextView)findViewById(R.id.altitudeTV);
+        bearingTV = (TextView)findViewById(R.id.bearingTV);
+        accuracyTV = (TextView)findViewById(R.id.accuracyTV);
+        callcountTV = (TextView)findViewById(R.id.callcountTV);
 
         
         
@@ -61,12 +76,18 @@ public class MainActivity extends Activity implements SensorEventListener {
         sensormanager.registerListener(this, accelerometerS, SensorManager.SENSOR_DELAY_NORMAL);
         sensormanager.registerListener(this, temperatureS, SensorManager.SENSOR_DELAY_NORMAL);
         sensormanager.registerListener(this, lightS, SensorManager.SENSOR_DELAY_NORMAL);
+        gpslistener = new MyGPSListener();
+        locman = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        locman.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, gpslistener);
 
-        callcount = 0;
+        callcount = BigInteger.valueOf(0);
 
     }
  
     public void updateAllGUIFields() {
+    	
+    	callcountTV.setText("Updates received: " + callcount.toString());
+    	
     	if (pressurevals != null) {
 	    	String text = "Pressure: ";
 	    	for(Float val: pressurevals) {
@@ -105,11 +126,19 @@ public class MainActivity extends Activity implements SensorEventListener {
 	    	}
 	    	lightTV.setText(text);
     	}
+    	if (gpsvals != null) {
+    		latitudeTV.setText("Latitude: " + Double.toString(gpsvals.get(0)));
+    		longitudeTV.setText("Longitude: " + Double.toString(gpsvals.get(1)));
+    		altitudeTV.setText("Altitude: " + Double.toString(gpsvals.get(2)));
+    		bearingTV.setText("Bearing: " + Double.toString(gpsvals.get(3)));
+    		accuracyTV.setText("Accuracy: " + Double.toString(gpsvals.get(4)));
+    	}
+    	
     	
     }
     
     public void onSensorChanged(SensorEvent event) {
-    	callcount += 1;
+    	callcount = callcount.add(BigInteger.valueOf(1));
     	if (event.sensor.getType() == Sensor.TYPE_PRESSURE) {
 			pressurevals = new ArrayList<Float>();
     		for(float val: event.values) {
@@ -150,6 +179,41 @@ public class MainActivity extends Activity implements SensorEventListener {
 	@Override
 	public void onAccuracyChanged(Sensor sensor, int accuracy) {
 		// TODO Auto-generated method stub
+		
+	}
+	
+	public class MyGPSListener implements LocationListener {
+
+		@Override
+		public void onLocationChanged(Location location) {
+	    	callcount = callcount.add(BigInteger.valueOf(1));
+			gpsvals = new ArrayList<Double>();
+			gpsvals.add(Double.valueOf(location.getLatitude()));
+			gpsvals.add(Double.valueOf(location.getLongitude()));	
+			gpsvals.add(Double.valueOf(location.getAltitude()));			
+			gpsvals.add(Double.valueOf(location.getBearing()));
+			gpsvals.add(Double.valueOf(location.getAccuracy()));
+			updateAllGUIFields();
+		}
+
+		@Override
+		public void onProviderDisabled(String provider) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void onProviderEnabled(String provider) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void onStatusChanged(String provider, int status, Bundle extras) {
+			// TODO Auto-generated method stub
+			
+		}
+		
 		
 	}
 
